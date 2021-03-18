02d8488044813f9dcd73e080c29ad7d619d7d9d6
go-ando <52556625+go-ando@users.noreply.github.com>
2021-03-18 16:09:47 +0900

    memo

--- src/components/molecules/user/UserIconwithName.jsx
+++ src/components/molecules/user/UserIconwithName.jsx
@@ -1,13 +1,19 @@
+import { useContext } from "react";
+import { memo } from "react";
 import styled from "styled-components";
-export const UserIconwithName = (props) => {
+import { UserContext } from "../../../providers/UserProvider";
+export const UserIconwithName = memo((props) => {
   const { image, name } = props;
+  const { userInfo } = useContext(UserContext);
+  const isAdmin = userInfo ? userInfo.isAdmin : false;
   return (
     <SContainer>
       <SImg width={160} height={160} src={image} alt={name} />
       <SName>{name}</SName>
+      {isAdmin && <SEdit>編集</SEdit>}
     </SContainer>
   );
-};
+});
 
 const SContainer = styled.div`
   text-align: center;
@@ -21,4 +27,9 @@ const SName = styled.p`
   font-weight: bold;
   margin: 0;
   color: #40514e;
+`;
+const SEdit = styled.span`
+  text-decoration: underline;
+  color: #aaa;
+  cursor: pointer;
 `;
